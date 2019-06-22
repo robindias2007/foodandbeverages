62ff88585d41b7023241cc6e12d52a357d4b6319
Robin Dias <robindias2007@gmail.com>
2019-06-23 00:05:36 +0530

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'foods#new'
   
 end
