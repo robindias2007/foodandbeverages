933a0eaeb0233581bb4012d438843339502c4c74
Robin Dias <robindias2007@gmail.com>
2019-07-20 20:54:39 +0530

    cuisines added

--- app/controllers/alcohols_controller.rb
+++ app/controllers/alcohols_controller.rb
@@ -1,6 +1,6 @@
 class AlcoholsController < ApplicationController
   before_action :set_alcohol, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin!, :only => [:index, :new, :create, :update, :index_edit]
+  before_action :authenticate_admin!, :only => [:new, :create, :update, :index_edit]
 
   # GET /alcohols
   # GET /alcohols.json
--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin!, :only => [:index, :new, :create, :update, :index_edit]
+  before_action :authenticate_admin!, :only => [:new, :create, :update, :index_edit]
 
   # GET /foods
   # GET /foods.json
